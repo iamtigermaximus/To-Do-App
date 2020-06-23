d4ef5541e64963c8f2577cd15e49347f813a8ed9
iamtigermaximus <46716968+iamtigermaximus@users.noreply.github.com>
2020-06-24 01:58:32 +0300

    Updates

---
+++ precache-manifest.f334faafdd4301f714d9eb24539dd2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698264624a4cfd8fdfecd78a2cf4dc6",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "aba7f32bd03db77f123b",
+    "url": "/To-Do-App/static/css/main.aadde6f8.chunk.css"
+  },
+  {
+    "revision": "c6abe6781c7afe9ad41d",
+    "url": "/To-Do-App/static/js/2.99d963e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.99d963e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba7f32bd03db77f123b",
+    "url": "/To-Do-App/static/js/main.5e724a4a.chunk.js"
+  },
+  {
+    "revision": "48e317fdc9e0c7e937fc",
+    "url": "/To-Do-App/static/js/runtime-main.4c451e14.js"
+  }
+]);
